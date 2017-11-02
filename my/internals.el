9c1d7874fe5c78bb23d7f674476de9af3f0292cb
shivekkhurana <shivekkhurana@wavewalker>
2017-11-02 14:03:22 +0530

    Configured ido

--- my/internals.el
+++ my/internals.el
@@ -5,3 +5,8 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Setup ido
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
--- my/packages.el
+++ my/packages.el
@@ -17,3 +17,27 @@
   :ensure t
   :pin melpa-stable)
 
+;; try
+(use-package try
+  :ensure t)
+
+;; multiple-cursors
+(use-package multiple-cursors
+  :ensure t
+  :commands (mc/add-cursor-on-click
+	     mc/edit-beginning-of-lines
+	     mc/edit-lines
+	     mc/insert-numbers
+	     mc/mark-all-dwim
+	     mc/mark-all-in-region-regexp
+	     mc/mark-all-like-this
+	     mc/mark-next-like-this
+	     mc/mark-previous-like-this
+	     mc/mark-sgml-tag-pair
+	     mc/reverse-regions
+	     mc/skip-to-next-like-this
+	     mc/skip-to-previous-like-this
+	     mc/sort-regions
+	     mc/unmark-next-like-this
+	     mc/unmark-previous-like-this)
+  )
